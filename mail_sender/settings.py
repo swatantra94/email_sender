9546987ea407aae3ed142eff26259ef352011967
swatantra94 <swatantranigam9451@gmail.com>
2021-01-29 11:15:15 +0530

    procfile added

--- mail_sender/settings.py
+++ mail_sender/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*q_^9=n&sib1njrk_um50!0=2jomk==8j$qq$%sd#5*^tcx2d1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['email-sender-swat.herokuapp.com','127.0.0.1']
 
 
 # Application definition
